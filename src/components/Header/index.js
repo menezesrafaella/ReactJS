3aeade046d437a7281eca23e2f603d09afdebe55
Rafaella Menezes <carmo.rafaella@yahoo.com.br>
2020-05-07 21:18:32 -0300

    ReactJS ok

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles.css"
+
+// Mesma coisa que na funcao abaixo, somente para ESTADO
+//class Header extends Component {
+//    render(){
+//        return <h1>Hello</h1>
+//    }
+//}
+
+
+const Header = () => <header id="main-header">JSHunt</header>
+
+
+export default Header;
